3ef60b4d43e9aa5c55918b70f33f52a9debb6543
T8\K.T <kosuke.jobs1996@gmail.com>
2025-02-01 17:42:18 +0900

    ログのソースを別ファイルに分割

--- main.py
+++ main.py
@@ -7,53 +7,7 @@ from logging.handlers import RotatingFileHandler
 import random
 import time
 import glob
-
-class SimpleRotatingFileHandler(logging.Handler):
-    def __init__(self, filename, maxBytes, encoding=None):
-        super().__init__()
-        self.filename = filename
-        self.maxBytes = maxBytes
-        self.encoding = encoding
-        self._open_file()
-
-    def _open_file(self):
-        self.stream = open(self.filename, 'a', encoding=self.encoding)
-
-    def emit(self, record):
-        if self.shouldRollover(record):
-            self.doRollover()
-        self.stream.write(self.format(record) + '\n')
-        self.stream.flush()
-
-    def shouldRollover(self, record):
-        return self.stream.tell() + len(self.format(record) + '\n') >= self.maxBytes
-
-    def doRollover(self):
-        self.stream.close()
-        self._open_file()  # ファイルを上書きモードで開き直す
-
-def configure_logging():
-    log_handler = SimpleRotatingFileHandler(
-        'scraping.log',  # 常に `scraping.log` というファイル名を使用
-        maxBytes=1 * 1024 * 1024,  # 1MB
-        encoding='utf-8'
-    )
-    log_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    log_handler.setFormatter(formatter)
-
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    logger.addHandler(log_handler)
-
-    logger.info('Logger is configured with simple rotating file handler.')
-    
-    return logger, log_handler
-
-def close_logging(logger):
-    for handler in logger.handlers:
-        handler.close()
-        logger.removeHandler(handler)
+from logging_config import configure_logging, close_logging
 
 logger, log_handler = configure_logging()
 
