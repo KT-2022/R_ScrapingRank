e91dc7b49fd7cb4b803b600b1493d53b96470803
T8\K.T <kosuke.jobs1996@gmail.com>
2025-03-02 23:53:52 +0900

    検索条件も登録できるようにアップデート

--- src/db_operations.py
+++ src/db_operations.py
@@ -12,36 +12,37 @@ def create_db():
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS items (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             shop_id TEXT,
             item_id TEXT,
             item_name TEXT,
-            PRIMARY KEY (shop_id, item_id)
+            search_word TEXT
         )
     ''')
     conn.commit()
     conn.close()
 
-def add_item_to_db(shop_id, item_id, item_name):
+def add_item_to_db(shop_id, item_id, item_name, search_word):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     cursor.execute('''
-        INSERT INTO items (shop_id, item_id, item_name)
-        VALUES (?, ?, ?)
-    ''', (shop_id, item_id, item_name))
+        INSERT INTO items (shop_id, item_id, item_name, search_word)
+        VALUES (?, ?, ?, ?)
+    ''', (shop_id, item_id, item_name, search_word))
     conn.commit()
     conn.close()
 
 def load_item_ids():
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute('SELECT shop_id, item_id, item_name FROM items')
+    cursor.execute('SELECT id, shop_id, item_id, item_name, search_word FROM items')
     item_ids = cursor.fetchall()
     conn.close()
     return item_ids
 
-def delete_item_from_db(shop_id, item_id):
+def delete_item_from_db(id):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute('DELETE FROM items WHERE shop_id = ? AND item_id = ?', (shop_id, item_id))
+    cursor.execute('DELETE FROM items WHERE id = ?', (id,))
     conn.commit()
     conn.close()
